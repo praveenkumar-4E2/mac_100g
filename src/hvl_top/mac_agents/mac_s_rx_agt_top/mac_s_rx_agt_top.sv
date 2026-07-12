492907735d87ae39e5d77d8bb6df74b8bd784e61
Bharani <kumar.ucs23@gmail.com>
2026-07-12 12:38:53 +0530

    feat(rx_agt_top): add slave rx_agent_top class declaration

---
+++ src/hvl_top/mac_agents/mac_s_rx_agt_top/mac_s_rx_agt_top.sv
@@ -0,0 +1,24 @@
+class mac_s_rx_agt_top_c extends uvm_agent;
+  `uvm_component_utils(mac_s_rx_agt_top_c)
+
+   extern function new (
+     string name = "mac_s_rx_agt_top_c",
+     uvm_component parent = null
+   );
+
+  extern function void build_phase(uvm_phase phase);
+endclass
+
+function mac_s_rx_agt_top_c::new( 
+  string name = "mac_s_rx_agt_top_c",
+  uvm_component parent = null
+ );
+  super.new(name,parent);
+  
+endfunction
+
+function void mac_s_rx_agt_top_c::build_phase(uvm_phase phase);
+  super.build_phase(phase);
+endfunction
+
+
