1f8fa9b2eb8f4f9c7d2d3fd3f674a9bb93b30dfd
rashid <rashidshaik372@gmail.com>
2026-07-12 12:16:36 +0530

    docs(seq):Add comments for master tx_seq class and methods

--- src/hvl_top/mac_agents/mac_m_tx_agt_top/mac_m_tx_seq.sv
+++ src/hvl_top/mac_agents/mac_m_tx_agt_top/mac_m_tx_seq.sv
@@ -1,3 +1,10 @@
+/**
+ * @brief MAC Master TX Base Sequence.
+ *
+ * Generates MAC transmit transactions and sends them to the
+ * master TX sequencer. This sequence creates the transaction
+ * object and defines the stimulus to be driven by the driver.
+ */
 class mac_m_tx_seq_c extends uvm_sequence #(mac_m_tx_xtn_c);
   `uvm_object_utils(mac_m_tx_seq_c)
 
@@ -9,12 +16,27 @@ class mac_m_tx_seq_c extends uvm_sequence #(mac_m_tx_xtn_c);
   extern task body();
 endclass
 
+/**
+ * @brief Constructor for the MAC Master TX sequence.
+ *
+ * Initializes the sequence object by calling the parent
+ * class constructor.
+ *
+ * @param name Name of the sequence object.
+ */
 function mac_m_tx_seq_c::new(
   string name="mac_m_tx_seq_c"
 );
   super.new(name);
 endfunction
 
+/**
+ * @brief Implements the main sequence behavior.
+ *
+ * Creates the MAC Master TX transaction using the UVM
+ * factory. Additional randomization and transaction
+ * execution can be added here.
+ */
 task mac_m_tx_seq_c::body();
   tx_xtn_h=mac_m_tx_xtn_c::type_id::create("tx_xtn_h");
 endtask
