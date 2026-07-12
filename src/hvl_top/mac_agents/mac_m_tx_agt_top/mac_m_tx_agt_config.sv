39e456934c04d6df2576330bd407c67f36b5d899
rashid <rashidshaik372@gmail.com>
2026-07-12 11:01:47 +0530

    feat(tx_agent_config):Add master tx_agent_config class declaration

---
+++ src/hvl_top/mac_agents/mac_m_tx_agt_top/mac_m_tx_agt_config.sv
@@ -0,0 +1,15 @@
+class mac_m_tx_agt_config_c extends uvm_object;
+  `uvm_object_utils(mac_m_tx_agt_config_c)
+
+  extern function new(
+    string name = "mac_m_tx_agt_config_c"
+  );
+ endclass
+
+function mac_m_tx_agt_config_c::new(
+  string name = "mac_m_tx_agt_config_c"
+);
+  super.new(name);
+endfunction
+
+
