7fbde5ed7341dbfeb8b142c0ca69c100ee1de860
Bharani <kumar.ucs23@gmail.com>
2026-07-12 12:35:16 +0530

    feat(rx_mon):add slave rx_monitor class declaration

---
+++ src/hvl_top/mac_agents/mac_s_rx_agt_top/mac_s_rx_mon.sv
@@ -0,0 +1,23 @@
+class mac_s_rx_mon_c extends uvm_monitor;
+  `uvm_component_utils(mac_s_rx_mon_c)
+
+   extern function new( 
+     string name = "mac_s_rx_mon_c",
+     uvm_component parent = null
+   );
+
+   extern function void build_phase(uvm_phase phase);
+
+endclass
+
+function mac_s_rx_mon_c::new(
+  string name = "mac_s_rx_mon_c",
+  uvm_component parent = null
+ );
+  super.new(name,parent);
+
+endfunction
+
+function void mac_s_rx_mon_c::build_phase(uvm_phase phase);
+  super.build_phase(phase);
+endfunction
