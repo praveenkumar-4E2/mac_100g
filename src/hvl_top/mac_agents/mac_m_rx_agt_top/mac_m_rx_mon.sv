2300dc951fdde6ce611b344e4a18c89fe6c4f591
haritha-52 <harithasetti2193@gmail.com>
2026-07-13 15:39:28 +0530

    refactor(comments): remove extra spaces and comments in master rx mon

--- src/hvl_top/mac_agents/mac_m_rx_agt_top/mac_m_rx_mon.sv
+++ src/hvl_top/mac_agents/mac_m_rx_agt_top/mac_m_rx_mon.sv
@@ -8,22 +8,17 @@
  * analysis components for checking and coverage.
  */
 class mac_m_rx_mon_c extends uvm_monitor;
-
-  /** Register the monitor with the UVM factory. */
   `uvm_component_utils(mac_m_rx_mon_c)
 
-  /** Constructor declaration. */
   extern function new(
     string name = "mac_m_rx_mon_c",
     uvm_component parent = null
   );
-
-  /** Build phase declaration. */
   extern function void build_phase(
     uvm_phase phase
   );
-endclass
 
+endclass
 
 /**
  * @brief Constructor implementation.
@@ -40,7 +35,6 @@ function mac_m_rx_mon_c::new(
 );
   super.new(name, parent);
 endfunction
-
 
 /**
  * @brief Build phase implementation.
