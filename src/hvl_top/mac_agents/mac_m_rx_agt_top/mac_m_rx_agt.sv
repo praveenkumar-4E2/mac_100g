a176db7be609f34433f5a78f243aa8c9de988e0b
haritha-52 <harithasetti2193@gmail.com>
2026-07-13 15:31:36 +0530

    refactor(comments): remove extra spaces and comments in master rx agt

--- src/hvl_top/mac_agents/mac_m_rx_agt_top/mac_m_rx_agt.sv
+++ src/hvl_top/mac_agents/mac_m_rx_agt_top/mac_m_rx_agt.sv
@@ -7,25 +7,21 @@
  * them to enable transaction-level communication.
  */
 class mac_m_rx_agt_c extends uvm_agent;
-
-  /** Register the agent with the UVM factory. */
   `uvm_component_utils(mac_m_rx_agt_c)
 
-  /** Constructor declaration. */
   extern function new(
     string name = "mac_m_rx_agt_c",
     uvm_component parent = null
   );
 
-  /** Build phase declaration. */
   extern function void build_phase(
     uvm_phase phase
   );
 
-  /** Connect phase declaration. */
   extern function void connect_phase(
     uvm_phase phase
   );
+  
 endclass
 
 
@@ -45,7 +41,6 @@ function mac_m_rx_agt_c::new(
   super.new(name, parent);
 endfunction
 
-
 /**
  * @brief Build phase implementation.
  *
@@ -59,7 +54,6 @@ function void mac_m_rx_agt_c::build_phase(
 );
   super.build_phase(phase);
 endfunction
-
 
 /**
  * @brief Connect phase implementation.
